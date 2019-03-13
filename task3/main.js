f3701f1d43faf0bf764666359080399ff6d4fb3b
Grzegorz Szermiński <szerman@poczta.fm>
2019-03-13 21:35:31 +0100

    Add task-3(for loop and forEach)

--- task3/main.js
+++ task3/main.js
@@ -1,17 +1,34 @@
 const btn = document.querySelector('button');
-element = document.querySelector('li');
+element = document.querySelectorAll('li');
+    //element = document.getElementsByTagName('li');
+        //dla forEach trzeba z tak pobranych elementów zrobić tablicę
+    //element = [...document.getElementsByTagName('li')];
 let fontGrow = 10;
 
+//FOREACH
 const chengeSize = () => {
-    debugger;
-    if( element.style.display = 'none') {
-        element.style.display = 'block'
-    } else {
-        element.style.fontSize = fontGrow + 'px';
-        fontGrow +=1; 
-        console.log('działa');
-        
+    element.forEach(item => {
+        if(item.style.display === '') {
+            item.style.display = 'block';
+            console.log('blok');
+        }
+        // item.style.fontSize = fontGrow + 'px';
+        item.style.fontSize = `${fontGrow}px`;
+    });
+    fontGrow++ 
     }
-}
+
+
+//FOR LOOP
+// const chengeSize = () => {
+//     for (let i = 0; i < element.length; i++) {
+//         // if(element[i].style.display === '') {
+//         if(!element[i].style.display) {    
+//             element[i].style.display = 'block';
+//         }
+//         element[i].style.fontSize = fontGrow + 'px';
+//     }
+//     fontGrow++;
+// }
 
 btn.addEventListener('click', chengeSize);
