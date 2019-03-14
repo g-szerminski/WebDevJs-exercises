8447b976649e282e6fe574e2edcb43a28b916f5e
Grzegorz Szermiński <szerman@poczta.fm>
2019-03-14 19:42:24 +0100

    task-4 - add ten elements of the list and the reset button

--- task4/main.js
+++ task4/main.js
@@ -3,22 +3,34 @@ let orderElement = 1;
 
 const init = () => {
     const btn = document.createElement('button');
-    const ulList = document.createElement('ul');
+    const btnReset = document.createElement('button');
+    const ul = document.createElement('ul');
     document.body.appendChild(btn);
-    document.body.appendChild(ulList);
-    btn.textContent = 'Stwórz listę';
+    document.body.appendChild(btnReset);
+    document.body.appendChild(ul);
+    btn.textContent = 'Dodaj 10 elementów do listy';
+    btnReset.textContent = 'Reset list';
+    btn.style.fontSize = '28px';
+    btnReset.style.fontSize = '28px';
     btn.addEventListener('click', createLiElements);
+    btnReset.addEventListener('click', cleanList);
 }
 
 const createLiElements = () => {
-    const ulList = document.querySelector('ul');
-    for (let i = 1; i <= 10 ; i++) {
-        const liElement = document.createElement('li');
-        document.ulList.appendChild(liElement);
-        orderElement++
+    for (let i = 0; i < 10; i++) {
+        const li = document.createElement('li');
+        li.textContent = `Element nr ${orderElement++}`;
+        // document.querySelector('ul').appendChild(li);
+        li.style.fontSize = `${size++}px`
+        const ulList = document.querySelector('ul');
+        ulList.appendChild(li);
+        // orderElement++;
     }
-    
 }
 
+const cleanList = () => {
+    document.querySelector('ul').textContent = '';
+    size = 10;
+    orderElement = 1;
+}
 init()
-
