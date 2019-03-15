7ed2e16c3faa16af45a5af62269e1840d7bd437b
Grzegorz Szermiński <szerman@poczta.fm>
2019-03-15 15:19:27 +0100

    task-6 - key down event with if and switch instructions

---
+++ task6/app.js
@@ -0,0 +1,43 @@
+    //INSTRUKCJA SWITCH
+//----------------------
+
+let red = 100;
+let green = 100;
+let blue = 100;
+document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+const changeColor = (e) => {
+    switch(e.keyCode) {
+        case 38: 
+            document.body.style.backgroundColor = `rgb(${red < 255 ? red++ : red}, ${green < 255 ? ++green : green}, ${blue < 255 ? blue++ : blue})`;
+            console.log(red);
+            
+            break;
+        case 40:
+            // document.body.style.backgroundColor = `rgb(${red--}, ${green--}, ${blue--})`;
+            document.body.style.backgroundColor = `rgb(${red > 0 ? red-- : red}, ${green > 0 ? --green : green}, ${blue > 0 ? blue-- : blue})`;
+            console.log(red);
+            break;
+    } 
+}
+window.addEventListener('keydown', changeColor);
+
+
+    //INSTRUKCJA IF
+//----------------------
+// let red = 100;
+// let green = 100;
+// let blue = 100;
+// document.body.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+// const changeColor = (e) => {
+//     if(e.keyCode === 38 && (red+green+blue) < 750) {
+//         document.body.style.backgroundColor = `rgb(${red++}, ${green++}, ${blue++})`;
+//     } else if(e.keyCode === 40 && (red+green+blue) > 0) {
+//         document.body.style.backgroundColor = `rgb(${red--}, ${green--}, ${blue--})`;   
+//     }
+//     console.log(red, green, blue);
+//     console.log(red+green+blue);
+// };
+// window.addEventListener('keydown', changeColor);
+
+
+  // console.log(e.keyCode, e.which);
