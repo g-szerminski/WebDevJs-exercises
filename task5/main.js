12a15918510f9292e5d2e891febf76fef8017574
Grzegorz Szermiński <szerman@poczta.fm>
2019-03-15 09:51:25 +0100

    task-5 - change bgc depends from clientX and clientY value

---
+++ task5/main.js
@@ -0,0 +1,44 @@
+// KOLOR GENEROWANY W ZALEŻNOŚCI OD TEGO GDZIE KLIKNIEMY
+
+// jeśli wartość dla x (event.clientX) i y (event.clientY) jest parzysta (np. 100,122)  to kolor czerwony
+// jesli wartość dla x i y jest nieparzysta to kolor niebieski
+// jeśli jeden z kolorów jest parzysty a drugi nieparzysty to kolor zielony
+document.body.addEventListener('click', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    if(x % 2 === 0 && y % 2 === 0) {
+        document.body.style.backgroundColor = 'red';
+    } else if(x % 2 !==0  && y % 2 !== 0) {
+        document.body.style.backgroundColor = 'blue';
+    } else {
+        document.body.style.backgroundColor = 'green';
+    }
+    console.log(x,y);
+});
+
+// INNE ROZWIĄZANIE
+
+// document.body.addEventListener('click', (event) => {
+//     console.log(event.clientX, event.clientY);
+//     const color = getColor(event);
+//     document.body.style.backgroundColor = color;
+//    })
+   
+//    const getColor = (e) => {
+//     if (e.clientX % 2 === 0) {
+//      if (e.clientY % 2 === 0) {
+//       return "red";
+//      } else {
+//       return "green";
+//      }
+//     }
+   
+//     else {
+//      if (e.clientY % 2 === 0) {
+//       return "green";
+//      } else {
+//       return "blue"
+//      }
+   
+//     }
+//    }
