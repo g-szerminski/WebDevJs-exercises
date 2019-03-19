e6f60e94884ac056eae264eeb0ba07cc0d0f99e0
Grzegorz Szermiński <szerman@poczta.fm>
2019-03-19 19:23:39 +0100

    project 2 - losowanie imienia + prefix

---
+++ Password-Message/project2-losowanie/app.js
@@ -0,0 +1,16 @@
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+const names = ['Jagienka', 'Dobrawa (Dobrusia)', 'Daria', 'Asia', 'Kira', 'Marysia', 'Anastazja', 'Kasia'];
+
+const prefixes = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę", "Szczerze uważam", "Na 100% twierdzę", "Jestem pewien"];
+
+const nameGenerator = () => {
+// const index = Math.floor(Math.random() * 7);
+const nameIndex = Math.floor(Math.random() * names.length);
+const prefixIndex = Math.floor(Math.random() * prefixes.length);
+div.textContent = `${prefixes[prefixIndex]}, że najfajniejsz imię to ${names[nameIndex]}`;
+
+}
+// console.log(names[index]);
+btn.addEventListener('click',nameGenerator);
