5716f4fc109bf9aabe2a1f04ed369ec9a07d9cff
Grzegorz Szermiński <szerman@poczta.fm>
2019-03-28 12:26:15 +0100

    14-licznik czasu spędzonego na stronie z użyciem closure

--- 14-licznik czasu spędzonego na stronie z użyciem closure/app.js
+++ 14-licznik czasu spędzonego na stronie z użyciem closure/app.js
@@ -1,1 +1,23 @@
-console.log('code');
+const timer = () => {
+    let time = 0;
+    const counter = () => {
+        time++;
+        document.body.textContent = `Na stronie spędziłeś ${time} s`;
+    }
+    return counter
+};
+const counting = timer();
+setInterval(counting, 1000);
+
+// const timer = () => {
+//     let time = 0;
+//     const counter = () => {
+//         setInterval(()=> {
+//         time++;
+//         document.body.textContent = `Na stronie spędziłeś ${time} s`;
+//         }, 1000);
+//     }
+//     return counter
+// };
+// const counting = timer();
+// counting();
