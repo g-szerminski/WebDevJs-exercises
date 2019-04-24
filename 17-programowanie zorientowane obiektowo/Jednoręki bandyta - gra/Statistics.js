0869e4b3146f689128b9b895b0a8384a5d8de4f1
Grzegorz Szermiński <szerman@poczta.fm>
2019-04-24 08:52:25 +0200

    add class Statistics with methods - work in progress

---
+++ 17-programowanie zorientowane obiektowo/Jednoręki bandyta - gra/Statistics.js
@@ -0,0 +1,26 @@
+class Statistics {
+    constructor() {
+        this.gameResults = [];
+    }
+    addGameToStatistics(win, bid) {
+        let gameResult = {
+            // win: win,
+            // bid: bid
+            win,
+            bid
+        }
+        console.log(gameResult);
+        this.gameResults.push(gameResult);
+    };
+    showGameStatistics() {
+        let games = this.gameResults.length;
+        let wins = this.gameResults.filter(result => result.win).length;
+        let losses = this.gameResults.filter(result => !result.win).length;
+        console.log(`ile razy grałeś: ${games}`);
+        console.log(`przegrane: ${wins}`);
+        console.log('wygrane: ' + losses);
+
+        // return [games, wins, losses];
+    }
+}
+const stats = new Statistics;
