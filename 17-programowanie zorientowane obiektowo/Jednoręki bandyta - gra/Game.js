30d2d72240a86821cc9f8d627b165da515fe1c26
Grzegorz Szermiński <szerman@poczta.fm>
2019-04-26 17:00:07 +0200

    add class Game

--- 17-programowanie zorientowane obiektowo/Jednoręki bandyta - gra/Game.js
+++ 17-programowanie zorientowane obiektowo/Jednoręki bandyta - gra/Game.js
@@ -1,1 +1,34 @@
-// link do gry: https://websamuraj.pl/examples/js/gra/ 
+// link do gry: https://websamuraj.pl/examples/js/gra/
+class Game {
+    constructor(start) {
+        this.stats = new Statistics();
+        this.wallet = new Wallet(start);
+        document.getElementById('start').addEventListener('click', this.startGame);
+        this.spanWallet = document.querySelector('.panel .wallet');
+        this.boards = document.querySelectorAll('div.color');
+        this.inputBid = document.getElementById('bid');
+        this.spanResult = document.querySelector('.score span.result');
+        this.spanGames = document.querySelector('.score span.number');
+        this.spanWins = document.querySelector('.score span.win');
+        this.spanLosses = document.querySelector('.score span.loss');
+        this.render();
+    }
+    render(colors = ['#2ee' , '#2ee', '#2ee'] ,money = this.wallet.getWalletValue(), result = '',stats = [0, 0, 0], bid = 0, wonMoney = 0) {
+        this.boards.forEach((item, index) => {
+            item.style.backgroundColor = colors[index];
+        });
+        this.spanWallet.textContent = money;
+        if(result) {
+            result = `Wygrałeś ${wonMoney}`;
+        } else if(!result && result != '') {
+            result = `Przegrałeś ${bid}`
+        }
+        this.spanResult.textContent = result;
+        this.spanGames.textContent = stats[0];
+        this.spanWins.textContent = stats[0];
+        this.spanLosses.textContent = stats[0];
+
+    }
+    startGame() {
+    }
+}
