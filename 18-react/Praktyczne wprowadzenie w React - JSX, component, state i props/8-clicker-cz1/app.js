4922fe84b77ac7e1e57656f2038b3bf3fa68eaa7
Grzegorz Szermiński <szerman@poczta.fm>
2019-05-06 11:54:20 +0200

    React exercises - work in progress

--- 18-react/Praktyczne wprowadzenie w React - JSX, component, state i props/8-clicker-cz1/app.js
+++ 18-react/Praktyczne wprowadzenie w React - JSX, component, state i props/8-clicker-cz1/app.js
@@ -1,49 +1,45 @@
 class Counter extends React.Component {
-
-  state = {
-    count: 0,
-    result: this.props.result
-  }
-
-  handleMathClick(type, number = 1) {
-    // debugger
-    if (type === "subtraction") {
-      this.setState(prevState => ({
-        count: prevState.count + 1,
-        result: prevState.result - number
-      }))
-    } else if (type === "reset") {
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: this.props.result
+    state = {
+        count: 0,
+        result: this.props.result
+    }
+    handleClick(type, number) {
+        // debugger
+        if(type === 'odejmowanie') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result - number
+            }))
+        } else if (type === 'reset') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                // result: prevState = 0
+                result: 0
+            }))
+        } else if(type === 'dodaj') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result + number
+            }))
         }
-      ))
-    } else if (type === "addition") {
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: prevState.result + number
-        }
-      ))
     }
 
-  }
-  render() {
-    return (
-      <>
-        <button onClick={this.handleMathClick.bind(this, "subtraction", 10)}>-10</button>
-        <button onClick={() => this.handleMathClick(1, "subtraction")}>-1</button>
-        <button onClick={this.handleMathClick.bind(this, "reset")}>Reset</button>
-        <button onClick={this.handleMathClick.bind(this, "addition", 1)}>+1</button>
-        <button onClick={this.handleMathClick.bind(this, "addition", 10)}>+10</button>
-        <h1>Liczba kliknięć: {this.state.count}</h1>
-        <h1>Wynik: {this.state.result}</h1>
-      </>
-    )
-  }
-
+    render() {
+        return (
+            <>
+                {/* <button onClick={this.handleClick.bind(this, 'odejmowanie', 10)}>-10</button> */}
+                {/* <button onClick={this.handleClick('odejmowanie', 10)}>-10</button> */}
+                <button onClick={() => this.handleClick('odejmowanie', 10)}>-10</button>
+                <button onClick={this.handleClick.bind(this, 'odejmowanie', 1)}>-1</button>
+                <button onClick={this.handleClick.bind(this, 'reset')}>Reset</button>
+                <button onClick={this.handleClick.bind(this, 'dodaj', 1)}>+1</button>
+                <button onClick={this.handleClick.bind(this, 'dodaj', 10)}>+10</button>
+                <h1>Liczba kliknięć: {this.state.count}</h1>
+                <h1>Wynik: {this.state.result}</h1>
+            </>
+        )
+    }
 }
-
-const startValue = 0;
-ReactDOM.render(<Counter result={startValue} />, document.getElementById('root'))
+const startValue = 10;
+// ReactDOM.render(<Counter result={10}/>, document.getElementById('root'));
+ReactDOM.render(<Counter result={startValue}/>, document.getElementById('root'));
--- 18-react/Praktyczne wprowadzenie w React - JSX, component, state i props/8-clicker-cz2/app.js
+++ 18-react/Praktyczne wprowadzenie w React - JSX, component, state i props/8-clicker-cz2/app.js
@@ -1,84 +1,75 @@
 class Counter extends React.Component {
-
-  state = {
-    count: 0,
-    result: this.props.result
-  }
-
-  handleMathClick = (type, number = 1) => {
-    // debugger
-    if (type === "subtraction") {
-      this.setState(prevState => ({
-        count: prevState.count + 1,
-        result: prevState.result - number
-      }))
-    } else if (type === "reset") {
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: this.props.result
+    state = {
+        count: 0,
+        result: this.props.result
+    }
+    handleClick = (type, number) => {
+        // debugger
+        if(type === 'odejmowanie') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result - number
+            }))
+        } else if (type === 'reset') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                // result: prevState = 0
+                result: 0
+            }))
+        } else if(type === 'dodaj') {
+            this.setState(prevState => ({
+                count: prevState.count + 1,
+                result: prevState.result + number
+            }))
         }
-      ))
-    } else if (type === "addition") {
-      this.setState(prevState => (
-        {
-          count: prevState.count + 1,
-          result: prevState.result + number
-        }
-      ))
     }
 
-  }
-  render() {
-    return (
-      <>
-        <MathButton
-          name="-10"
-          number="10"
-          type="subtraction"
-          click={this.handleMathClick}
-        />
-        <MathButton
-          name="-1"
-          number="1"
-          type="subtraction"
-          click={this.handleMathClick}
-        />
+    render() {
+        return (
+            <>
+                <MathButton
+                    name='-10'
+                    number='10'
+                    type='odejmowanie'
+                    // click={this.handleClick.bind(this)}
+                    click={this.handleClick}
+                />
+                <MathButton
+                    name='-1'
+                    number='1'
+                    type='odejmowanie'
+                    click={this.handleClick}
+                />
+                <MathButton
+                    name='reset'
+                    type='reset'
+                    click={this.handleClick}
+                />
+                <MathButton
+                    name='+1'
+                    number='1'
+                    type='dodaj'
+                    click={this.handleClick}
+                /><MathButton
+                name='+10'
+                number='10'
+                type='dodaj'
+                click={this.handleClick}
+            />
 
-        <MathButton
-          name="reset"
-          type="reset"
-          click={this.handleMathClick}
-        />
-
-        <MathButton
-          name="+1"
-          number="1"
-          type="addition"
-          click={this.handleMathClick}
-        />
-
-        <MathButton
-          name="+10"
-          number="10"
-          type="addition"
-          click={this.handleMathClick}
-        />
-        <h1>Liczba kliknięć: {this.state.count}</h1>
-        <h1>Wynik: {this.state.result}</h1>
-      </>
-    )
-  }
-
+                <h1>Liczba kliknięć: {this.state.count}</h1>
+                <h1>Wynik: {this.state.result}</h1>
+            </>
+        )
+    }
 }
-
 const MathButton = (props) => {
-  const number = parseInt(props.number)
-  return (
+    const number = parseInt(props.number);
+    return (
     <button onClick={() => props.click(props.type, number)}>{props.name}</button>
-  )
-}
+    )
+};
 
-
-const startValue = 0;
-ReactDOM.render(<Counter result={startValue} />, document.getElementById('root'))
+const startValue = 10;
+// ReactDOM.render(<Counter result={10}/>, document.getElementById('root'));
+ReactDOM.render(<Counter result={startValue}/>, document.getElementById('root'));
