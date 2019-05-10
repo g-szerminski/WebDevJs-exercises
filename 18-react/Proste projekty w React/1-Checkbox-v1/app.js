0d655e9b300dbebc0489ec75563f90cb339dc1ed
Grzegorz Szermiński <szerman@poczta.fm>
2019-05-10 23:02:16 +0200

    add react exercises content

---
+++ 18-react/Proste projekty w React/1-Checkbox-v1/app.js
@@ -0,0 +1,40 @@
+const PositveMessage = () => <p>Możesz obejżeć film. Zapraszamy.</p>;
+const NegativeMessage = () => <p>Nie możesz obejżeć filmu.</p>;
+
+class CheckBoxAge extends React.Component {
+  state = {
+    isConfirmed: false
+  }
+
+  heandleCheckBox = () => {
+    this.setState({
+      isConfirmed: !this.state.isConfirmed
+    })
+  }
+
+  displayMessage = () => {
+    if (this.state.isConfirmed) {
+      return <PositiveMessage />;
+    } else {
+      return <NegativeMessage />;
+    }
+  }
+
+  render() {
+    console.log(this.state.isConfirmed);
+    return (
+      <>
+        <h1>Kup bilet na horror roku !</h1>
+        <input
+          type="checkbox"
+          id="age"
+          onChange={this.heandleCheckBox}
+          checked={this.state.isConfirmed}
+        />
+        <label htmlFor="age">Mam conajmniej 16 lat</label>
+        {this.displayMessage()}
+      </>
+    )
+  }
+}
+ReactDOM.render(<CheckBoxAge />, document.getElementById("root"));
