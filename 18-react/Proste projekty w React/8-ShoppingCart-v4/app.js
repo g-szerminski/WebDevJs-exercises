f726e5e84ddbd0b4f5cdb76b397f538de7e059cc
Grzegorz Szermiński <szerman@poczta.fm>
2019-05-13 07:51:39 +0200

    add React exercises - Shopping Cart(code destructuring)

--- 18-react/Proste projekty w React/8-ShoppingCart-v4/app.js
+++ 18-react/Proste projekty w React/8-ShoppingCart-v4/app.js
@@ -1,46 +1,55 @@
-// destrukturyzacja
-
 class App extends React.Component {
   state = {
     availableProducts: 7,
-    shoppingCart: 1,
-  }
+    shoppingCart: 0
+  };
 
-  handleRemoveFromCart = () => {
+  handleRemoveCartItem = () => {
     this.setState({
-      shoppingCart: this.state.shoppingCart - 1,
-    })
-  }
+      shoppingCart: this.state.shoppingCart - 1
+      // availableProducts: this.state.availableProducts + 1
+    });
+  };
 
-  handleAddToCart = () => {
+  handleAddCartItem = () => {
     this.setState({
-      shoppingCart: this.state.shoppingCart + 1,
-    })
-  }
+      shoppingCart: this.state.shoppingCart + 1
+      // availableProducts: this.state.availableProducts - 1
+    });
+  };
 
   handleBuy = () => {
     this.setState({
       availableProducts: this.state.availableProducts - this.state.shoppingCart,
-      shoppingCart: 0,
-    })
-  }
+      shoppingCart: 0
+    });
+  };
 
   render() {
-    const { shoppingCart, availableProducts } = this.state
+    const { shoppingCart, availableProducts } = this.state;
     const style = shoppingCart === 0 ? { opacity: 0.3 } : {};
-
     return (
       <div>
-        <button disabled={shoppingCart ? false : true} onClick={this.handleRemoveFromCart}>-</button>
-
-        <span style={style}> {shoppingCart} </span>
-
-        <button disabled={shoppingCart === availableProducts ? true : false} onClick={this.handleAddToCart}>+</button>
+        <button
+          disabled={shoppingCart ? false : true}
+          onClick={this.handleRemoveCartItem}>
+          -
+        </button>
+        <span style={style}>{shoppingCart}</span>
+        <button
+          disabled={shoppingCart === availableProducts ? true : false}
+          onClick={this.handleAddCartItem}>
+          +
+        </button>
         {shoppingCart > 0 && <button onClick={this.handleBuy}>Kup</button>}
-
+        {/* <button
+          disabled={this.state.availableProducts ? false : true}
+          onClick={this.handleAddCartItem}>
+          +
+        </button> */}
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
