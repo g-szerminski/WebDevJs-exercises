2991f8946cc183b1de4ead247b6e84c8d00ac9ad
Grzegorz Szermiński <szerman@poczta.fm>
2019-05-15 18:10:59 +0200

    add React exercises-work in progress

--- 18-react/Proste projekty w React/12-ListItems-v4/app.js
+++ 18-react/Proste projekty w React/12-ListItems-v4/app.js
@@ -4,52 +4,49 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
-      sex: "male",
+      sex: "male"
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
-      sex: "female",
+      sex: "female"
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
-      sex: "female",
+      sex: "female"
     },
     {
       id: 4,
-      age: 24,
+      age: 39,
       name: "Karol",
-      sex: "male",
+      sex: "male"
     }
   ]
-}
+};
 
-const Item = ({ user }) => (
+const Item = ({ content }) => (
   <div className="userInfo">
-    <h1>{user.name}</h1>
-    <p>Informacje o użytkowniku</p>
-    <p>Wiek użytkownika: <strong>{user.age}</strong></p>
-    <p>Płeć użytkownika: {user.sex}</p>
+    <h1>{content.name}</h1>
+    <p>Inforamcje o użytkowniku:</p>
+    <p>Wiek użytkownika: {content.age}</p>
+    <p>Płeć: {content.sex}</p>
   </div>
-)
+);
+
 class ListItems extends React.Component {
-  state = {
-
-  }
+  // state = {
+  //   items: ["jabłko", "sliwka", "gruszka"]
+  // };
   render() {
-    let users = this.props.data.users
-    users = users.filter((user) => user.sex === "male")
-    const Items = users.map(user => <Item key={user.id} user={user} />)
+    let users = this.props.data.users;
+    users = users.filter(user => user.sex === "female");
+    const Items = users.map(item => <Item key={item.id} content={item} />);
 
-    return (
-      <ul>
-        {Items}
-      </ul>
-    )
+    return <ul>{Items}</ul>;
   }
 }
 
-ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
--- 18-react/Proste projekty w React/13-ListItems-v5/app.js
+++ 18-react/Proste projekty w React/13-ListItems-v5/app.js
@@ -4,75 +4,57 @@ const data = {
       id: 1,
       age: 29,
       name: "Arek",
-      sex: "male",
+      sex: "male"
     },
     {
       id: 2,
       age: 49,
       name: "Marta",
-      sex: "female",
+      sex: "female"
     },
     {
       id: 3,
       age: 19,
       name: "Stasia",
-      sex: "female",
+      sex: "female"
     },
     {
       id: 4,
-      age: 24,
+      age: 39,
       name: "Karol",
-      sex: "male",
+      sex: "male"
     }
   ]
-}
+};
 
-const Item = ({ user }) => (
+const Item = ({ content }) => (
   <div className="userInfo">
-    <h1>{user.name}</h1>
-    <p>Informacje o użytkowniku</p>
-    <p>Wiek użytkownika: <strong>{user.age}</strong></p>
-    <p>Płeć użytkownika: {user.sex}</p>
+    <h1>{content.name}</h1>
+    <p>Inforamcje o użytkowniku:</p>
+    <p>Wiek użytkownika: {content.age}</p>
+    <p>Płeć: {content.sex}</p>
   </div>
-)
+);
+
 class ListItems extends React.Component {
-  state = {
-    select: "all",
-  }
-
-  handleUsersFilter(option) {
-    this.setState({
-      select: option
-    })
-
-  }
+  state = {};
 
   usersList = () => {
     let users = this.props.data.users;
-    switch (this.state.select) {
-      case "all":
-        return users.map(user => <Item user={user} key={user.id} />)
-      case "female":
-        users = users.filter(user => user.sex === "female");
-        return users.map(user => <Item user={user} key={user.id} />)
-      case "male":
-        users = users.filter(user => user.sex === "male");
-        return users.map(user => <Item user={user} key={user.id} />)
-      default:
-        return "coś się zepsuło"
+    users = users.map(item => <Item content={item} key={item.id} />);
+    return users;
+  };
 
-    }
-  }
   render() {
     return (
       <div>
-        <button onClick={this.handleUsersFilter.bind(this, "all")}>Wszyscy</button>
-        <button onClick={this.handleUsersFilter.bind(this, "female")}>Kobiety</button>
-        <button onClick={this.handleUsersFilter.bind(this, "male")}>Mężczyźni</button>
+        <button>Wszyscy</button>
+        <button>Mężczyźni</button>
+        <button>Kobiety</button>
         {this.usersList()}
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<ListItems data={data} />, document.getElementById('root'))
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
