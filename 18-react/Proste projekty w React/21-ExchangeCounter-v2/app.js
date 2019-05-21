30d0436c65ef526018c925c55c4cab19a21d0c4c
Grzegorz Szermiński <szerman@poczta.fm>
2019-05-21 09:42:07 +0200

    add React exercises-exchange couner

--- 18-react/Proste projekty w React/21-ExchangeCounter-v2/app.js
+++ 18-react/Proste projekty w React/21-ExchangeCounter-v2/app.js
@@ -1,15 +1,3 @@
-// const Dollars = props => (
-//   <div>
-//     Wartość w dolarach:{" "}
-//     {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}
-//   </div>
-// );
-
-// const Euros = props => {
-//   const value = (props.cash / props.ratio).toFixed(2);
-//   return <div>Wartość w euro: {props.cash <= 0 ? "" : value}</div>;
-// };
-
 const Cash = props => {
   const value = (props.cash / props.ratio).toFixed(2);
   return (
@@ -21,10 +9,29 @@ const Cash = props => {
 
 class ExchangeCounter extends React.Component {
   state = {
-    amount: "",
-    ratioDollar: 3.6,
-    ratioEuro: 4.2
+    amount: ""
   };
+
+  currencies = [
+    {
+      id: 1,
+      name: "dollar",
+      ratio: 3.6,
+      title: "Wartość w dolarach"
+    },
+    {
+      id: 2,
+      name: "euro",
+      ratio: 4.2,
+      title: "Wartość w euro"
+    },
+    {
+      id: 3,
+      name: "pund",
+      ratio: 4.5,
+      title: "Wartość w funtach"
+    }
+  ];
 
   handleChange = e => {
     this.setState({
@@ -33,20 +40,25 @@ class ExchangeCounter extends React.Component {
   };
 
   render() {
-    const { amount, ratioDollar, ratioEuro } = this.state;
+    const { amount } = this.state;
+    const exchange = this.currencies.map(currency => (
+      <Cash
+        key={currency.id}
+        ratio={currency.ratio}
+        title={currency.title}
+        cash={amount}
+      />
+    ));
     return (
-      <div className="app">
+      <div className='app'>
         <label>
           <input
-            type="number"
+            type='number'
             value={this.state.amount}
             onChange={this.handleChange}
           />
         </label>
-        {/* <Dollars cash={amount} ratio={ratioDollar} />
-        <Euros cash={amount} ratio={ratioEuro} /> */}
-        <Cash cash={amount} ratio={ratioDollar} title="Wartość w dolarach" />
-        <Cash cash={amount} ratio={ratioEuro} title="Wartość w euro" />
+        {exchange}
       </div>
     );
   }
--- 18-react/Proste projekty w React/22-ExchangeCounter-v3/app.js
+++ 18-react/Proste projekty w React/22-ExchangeCounter-v3/app.js
@@ -1,109 +1,108 @@
-const Cash = (props) => {
-  const value = (props.cash / props.ratio * props.price).toFixed(2)
+const Cash = props => {
+  const value = ((props.cash / props.ratio) * props.price).toFixed(2);
   return (
-    <div>{props.title} {props.cash <= 0 ? "" : value}</div>
-  )
-}
+    <div>
+      {props.title} {props.cash <= 0 ? "" : value}
+    </div>
+  );
+};
 
 class ExchangeCounter extends React.Component {
-
   state = {
     amount: "",
-    product: "gas"
-  }
+    product: "electricity"
+  };
 
   static defaultProps = {
     currencies: [
       {
         id: 0,
-        name: 'zloty',
+        name: "złoty",
         ratio: 1,
-        title: 'Wartość w złotówkach:'
+        title: "Wartość w złotówkach"
       },
       {
         id: 1,
-        name: 'dollar',
+        name: "dollar",
         ratio: 3.6,
-        title: 'Wartość w dolarach:'
+        title: "Wartość w dolarach"
       },
       {
         id: 2,
-        name: 'euro',
-        ratio: 4.1,
-        title: 'Wartość w euro:'
+        name: "euro",
+        ratio: 4.2,
+        title: "Wartość w euro"
       },
       {
         id: 3,
-        name: 'pound',
-        ratio: 4.55,
-        title: 'Wartość w funtach:'
-      },
+        name: "pund",
+        ratio: 4.5,
+        title: "Wartość w funtach"
+      }
     ],
     prices: {
-      electricity: .51,
-      gas: 4.76,
-      oranges: 3.79,
+      electricity: 0.5,
+      gas: 4.9,
+      oranges: 3
     }
-  }
-
+  };
 
   handleChange = e => {
     this.setState({
       amount: e.target.value
-    })
-  }
-
+    });
+  };
   handleSelect = e => {
     this.setState({
       product: e.target.value,
-      amount: "",
-    })
-  }
-
+      amount: ""
+    });
+  };
   insertSuffix(select) {
-    if (select === "electricity") return <em>kWh</em>
-    else if (select === "gas") return <em>litrów</em>
-    else if (select === "oranges") return <em>kilogramów</em>
-    else return null
+    if (select === "electricity") return <em>kWh</em>;
+    else if (select === "gas") return <em>litrów</em>;
+    else if (select === "oranges") return <em>kg</em>;
+    else return null;
   }
-
   selectPrice(select) {
-    const price = this.props.prices[select]
-    return price
+    return this.props.prices[select];
   }
 
   render() {
-
     const { amount, product } = this.state;
-    const price = this.selectPrice(product)
-
-    const calculators = this.props.currencies.map(currency => (
-      <Cash key={currency.id} ratio={currency.ratio} title={currency.title} cash={amount} price={price} />
-    ))
-
+    const price = this.selectPrice(product);
+    const exchange = this.props.currencies.map(currency => (
+      <Cash
+        key={currency.id}
+        ratio={currency.ratio}
+        title={currency.title}
+        cash={amount}
+        price={price}
+      />
+    ));
     return (
-      <div className="app">
-        <label>Wybierz produkt:
-        <select value={product} onChange={this.handleSelect}>
-            <option value="electricity">prąd</option>
-            <option value="gas">benzyna</option>
-            <option value="oranges">pomarańcze</option>
+      <div className='app'>
+        <label>
+          Wybierz produkt:
+          <select value={product} onChange={this.handleSelect}>
+            <option value='electricity'>prąd</option>
+            <option value='gas'>banzyna</option>
+            <option value='oranges'>pomarańcze</option>
           </select>
-
         </label>
         <br />
         <label>
           <input
-            type="number"
+            type='number'
             value={this.state.amount}
             onChange={this.handleChange}
           />
           {this.insertSuffix(this.state.product)}
         </label>
-        {calculators}
+        {exchange}
       </div>
-    )
-
+    );
   }
 }
-ReactDOM.render(<ExchangeCounter />, document.getElementById('root'))
+
+ReactDOM.render(<ExchangeCounter />, document.getElementById("root"));
