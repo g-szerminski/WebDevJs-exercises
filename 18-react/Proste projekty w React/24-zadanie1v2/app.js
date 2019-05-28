9cd456a783a589e23692dd238b25e4f8f664dfd4
Grzegorz Szermiński <szerman@poczta.fm>
2019-05-28 09:17:58 +0200

    add React task - 'Random fortune'

--- 18-react/Proste projekty w React/24-zadanie1v2/app.js
+++ 18-react/Proste projekty w React/24-zadanie1v2/app.js
@@ -1,17 +1,9 @@
-/* https://websamuraj.pl/examples/react/zadanie-wrozba/ */
-//Zobacz
-
-//WYTYCZNE
-// Jeden komponent
-// Wersja na 5 - zobacz wróżbę, losuje jedną z trzech wróżb
-// Wersja na 6 - dodaje do trzech istniejących wróżb kolejną
-
 class RandomFortune extends React.Component {
   state = {
     fortunes: [
-      "Będziesz długo żyć",
-      "Będziesz żyć nie za długo :-(",
-      "Sama właściwie nie wiem co będzie"
+      " Będziesz długo żyć",
+      " Będziesz żyć nie za długo :-(",
+      " Sama właściwie nie wiem co będzie"
     ],
     fortune: null,
     value: ""
@@ -31,28 +23,46 @@ class RandomFortune extends React.Component {
   };
 
   handelAddOmen = () => {
-    if (this.state.value === "") alert("Wpisz coś");
-    const fortunes = this.state.fortunes.push(this.state.value);
+    if (this.state.value === "") return alert("Wpisz coś");
+    const fortunes = [...this.state.fortunes];
+    fortunes.push(`${""} ${this.state.value}`);
     this.setState({
       fortunes,
       value: ""
     });
-    alert(`Wróżba dodana. Aktualne wróżby to: ${this.state.fortunes}`);
+    alert(
+      `Dodana wróżba to: ${this.state.value}
+      Aktualne wróżby to: ${fortunes}`
+    );
   };
+
+  // handelAddOmen = () => {
+  //   if (this.state.value === "") return alert("Wpisz coś");
+  //   this.state.fortunes.push(this.state.value);
+  //   this.setState({
+  //     value: ""
+  //   });
+  //   alert(`Dodana wróżba to: ${this.state.value}`);
+  // };
 
   render() {
     return (
       <div>
         <button onClick={this.handleRandomOmen}>Zobacz wróżbę</button>
+        <br />
         <label>
           <input
-            type='text'
+            type="text"
             value={this.state.value}
             onChange={this.handleInputOmen}
           />
           <button onClick={this.handelAddOmen}>Dodaj wróżbę</button>
         </label>
-        <h1>{this.state.fortune ? this.state.fortune : "losuj wreszcie !"}</h1>
+        {this.state.fortune ? (
+          <h1>{this.state.fortune}</h1>
+        ) : (
+          " losuj wreszcie !"
+        )}
       </div>
     );
   }
