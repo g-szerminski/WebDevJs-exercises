74822159735e72e96446bd62cad12296fe9448f7
Grzegorz Szermiński <szerman@poczta.fm>
2019-06-03 10:37:04 +0200

    add React exercises 'component life cycle'- work in progress

--- 18-react/Zaawansowane zagadnienia/1-setState-examples/app.js
+++ 18-react/Zaawansowane zagadnienia/1-setState-examples/app.js
@@ -2,72 +2,52 @@ class Clicker extends React.Component {
   state = {
     number1: 0,
     number2: 0,
-    number3: 0,
-  }
+    number3: 0
+  };
 
   handleClick1 = () => {
     this.setState({
-      number1: this.state.number1 + 1,
-    })
-    console.log(this.state.number1 + "w metodzie handleClick");
-  }
+      number1: this.state.number1 + 1
+    });
+  };
 
   handleClick2 = () => {
     this.setState({
-      number2: this.state.number2 + 1,
-    })
+      number2: this.state.number2 + 1
+    });
     this.setState({
-      number2: this.state.number2 + 1,
-    })
+      number2: this.state.number2 + 1
+    });
     this.setState({
-      number2: this.state.number2 + 1,
-    })
-    // console.log(this.state.number2 + "w metodzie handleClick");
-
-  }
+      number2: this.state.number2 + 1
+    });
+  };
 
   handleClick3 = () => {
-    this.setState((state) => {
-      // console.log(state);
+    this.setState(() => {
       return {
-        number3: state.number3 + 1,
-      }
-    }
-    )
-
-    this.setState((state) => {
-      // console.log(state);
+        number3: this.state.number3 + 1
+      };
+    });
+    this.setState(prevState => {
       return {
-        number3: state.number3 + 1,
-      }
-    }
-    )
-    this.setState((prevState) => {
-      console.log(prevState);
-      // console.log(this.state);
-      return {
-        number3: prevState.number3 + 1,
-      }
-    }
-    )
-    console.log(this.state.number3);
-  }
-
+        number3: prevState.number3 + 3
+      };
+    });
+  };
 
   render() {
-    // console.log(this.state.number2 + "w render");
     return (
       <div>
         <button onClick={this.handleClick1}>Podbij o 1</button>
         <h1>{this.state.number1}</h1>
         <button onClick={this.handleClick2}>Podbij o 3</button>
         <h1>{this.state.number2}</h1>
-        <button onClick={this.handleClick3}>Podbij o 3</button>
+        <button onClick={this.handleClick3}>Podbij o 4</button>
         <h1>{this.state.number3}</h1>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<Clicker />, document.getElementById('root'))
-
+ReactDOM.render(<Clicker />, document.getElementById("root"));
--- 18-react/Zaawansowane zagadnienia/2-lifecycle-metody/app.js
+++ 18-react/Zaawansowane zagadnienia/2-lifecycle-metody/app.js
@@ -1,60 +1,53 @@
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       number: 0
-    }
-    console.log("Wywołać metodę constructor");
+    };
+    console.log("Wywołana metoda constructor");
   }
 
   componentWillMount() {
-    console.log('Wywołana componentWillMount');
+    console.log("Wywołanie metody WillMount");
   }
 
   componentDidMount() {
-    console.log('Wywołana componentDidMount');
+    console.log("Wywołanie metody DidMount");
     this.setState({
       number: 1
-    })
+    });
   }
 
   componentDidUpdate() {
-    console.log('Wywołana componentDidUpdate');
+    console.log("Wywołanie metody DidUpdate");
   }
 
   render() {
     console.log("Wywołana metoda render");
-
     return (
-      <div>
-        <p>
-          lifecycle - montowanie komponentu
-      </p>
+      <>
+        <p>lifecycle - montowanie komponentu</p>
         <Child />
-      </div>
-    )
+      </>
+    );
   }
-
-
 }
 
 class Child extends React.Component {
   componentDidMount() {
-    console.log('Wywołana componentDidMount w komponencie Child');
+    console.log("Wywołanie metody DidMount w komponencie Child");
     // this.setState({
     //   number: 1
-    // })
+    // });
   }
-
   render() {
     console.log(`
-    -----------------------------
+    -------------------------------------------
     Wywołana metoda render w komponencie Child
+    -------------------------------------------
     `);
-    return (
-      <h1>Dziecko</h1>
-    )
+    return <h1>Dziecko</h1>;
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
